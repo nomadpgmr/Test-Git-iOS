22965814792812670f65230774dc3b7b5d5f6e70
nomadpgmr <nomadpgmr@hotmail.com>
2014-05-20 15:36:06 -0700

    Initial Commit

---
+++ Git Test/RWViewController.h
@@ -0,0 +1,13 @@
+//
+//  RWViewController.h
+//  Git Test
+//
+//  Created by Roger Weiss on 5/20/14.
+//  Copyright (c) 2014 Roger Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RWViewController : UIViewController
+
+@end
---
+++ Git Test/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Git Test
+//
+//  Created by Roger Weiss on 5/20/14.
+//  Copyright (c) 2014 Roger Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RWAppDelegate class]));
+    }
+}
