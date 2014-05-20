72aecddd14190da43cd359af245f5ad893e75806
nomadpgmr <nomadpgmr@hotmail.com>
2014-05-20 15:43:45 -0700

    added test class

---
+++ Git Test/RWTest.h
@@ -0,0 +1,13 @@
+//
+//  RWTest.h
+//  Git Test
+//
+//  Created by Roger Weiss on 5/20/14.
+//  Copyright (c) 2014 Roger Weiss. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RWTest : NSObject
+
+@end
---
+++ Git Test/RWTest.m
@@ -0,0 +1,13 @@
+//
+//  RWTest.m
+//  Git Test
+//
+//  Created by Roger Weiss on 5/20/14.
+//  Copyright (c) 2014 Roger Weiss. All rights reserved.
+//
+
+#import "RWTest.h"
+
+@implementation RWTest
+
+@end
